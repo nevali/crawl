bc1ffaf188b3689e2ecf8fbdddfd1a3b64dbcfd1
Mo McRoberts <mo@nevali.net>
2013-06-23 12:17:58 +0100

    Add commit and rollback mechanisms; rename private symbols.

--- cache.c
+++ cache.c
@@ -20,13 +20,16 @@
 
 #include "p_libcrawl.h"
 
+static int cache_create_dirs_(CRAWL *crawl, const char *path);
+static int cache_copy_filename_(CRAWL *crawl, const CACHEKEY key, const char *type, int temporary);
+
 int
-crawl_cache_key(CRAWL *crawl, CACHEKEY dest, const char *uri)
+crawl_cache_key_(CRAWL *crawl, CACHEKEY dest, const char *uri)
 {
 	unsigned char buf[SHA256_DIGEST_LENGTH];
 	size_t c;
 	
-    (void) crawl;
+	(void) crawl;
     
 	/* The cache key is a truncated SHA-256 of the URI */
 	SHA256((const unsigned char *) uri, strlen(uri), buf);
@@ -38,73 +41,196 @@ crawl_cache_key(CRAWL *crawl, CACHEKEY dest, const char *uri)
 }
 
 size_t
-cache_filename(CRAWL *crawl, const CACHEKEY key, const char *type, char *buf, size_t bufsize)
+cache_filename_(CRAWL *crawl, const CACHEKEY key, const char *type, char *buf, size_t bufsize, int temporary)
 {
-    size_t needed;
-    
-    (void) crawl;
-    
-    if(buf)
-    {
-        *buf = 0;
-    }
-    /* base path + "/" + key[0..1] + "/" + key[2..3] + "/" + key[0..n] + "." + type + ".tmp" */
-    needed = 1 + 2 + 1 + 2 + 1 + strlen(key) + 1 + strlen(type) + 4 + 1;
-    if(!buf || needed < bufsize)
-    {
-        return needed;
-    }
-    sprintf(buf, "%c%c/%c%c/%s.%s.tmp", key[0], key[1], key[2], key[3], key, type);
-    return needed;
+	size_t needed;
+	const char *suffix;
+
+	if(buf)
+	{
+		*buf = 0;
+	}
+	/* base path + "/" + key[0..1] + "/" + key[2..3] + "/" + key[0..n] + "." + type + ".tmp" */
+	needed = strlen(crawl->cache) + 1 + 2 + 1 + 2 + 1 + strlen(key) + 1 + strlen(type) + 4 + 1;
+	if(!buf || needed > bufsize)
+	{
+		return needed;
+	}
+	if(temporary)
+	{
+		suffix = CACHE_TMP_SUFFIX;
+	}
+	else
+	{
+		suffix = "";
+	}
+	sprintf(buf, "%s/%c%c/%c%c/%s.%s%s", crawl->cache, key[0], key[1], key[2], key[3], key, type, suffix);
+	return needed;
 }
 
 FILE *
-cache_open_info_write(CRAWL *crawl, const CACHEKEY key)
+cache_open_info_write_(CRAWL *crawl, const CACHEKEY key)
 {
-    size_t needed;
-    char *p;
-    
-    needed = cache_filename(crawl, key, CACHE_INFO_SUFFIX, NULL, 0);
-    if(needed > crawl->cachefile_len)
-    {
-        p = (char *) realloc(crawl->cachefile, needed);
-        if(!p)
-        {
-            return NULL;
-        }
-        crawl->cachefile = p;
-        crawl->cachefile_len = needed;
-    }
-    /* XXX create directory structure */
-    if(cache_filename(crawl, key, CACHE_INFO_SUFFIX, crawl->cachefile, needed) != needed)
-    {
-        return NULL;
-    }
-    return fopen(crawl->cachefile, "w");
+	if(cache_copy_filename_(crawl, key, CACHE_INFO_SUFFIX, 1))
+	{
+		return NULL;
+	}
+	if(cache_create_dirs_(crawl, crawl->cachefile))
+	{
+		return NULL;
+	}
+	return fopen(crawl->cachefile, "w");
 }
 
 FILE *
-cache_open_payload_write(CRAWL *crawl, const CACHEKEY key)
+cache_open_payload_write_(CRAWL *crawl, const CACHEKEY key)
 {
-    size_t needed;
-    char *p;
-    
-    needed = cache_filename(crawl, key, CACHE_PAYLOAD_SUFFIX, NULL, 0);
-    if(needed > crawl->cachefile_len)
-    {
-        p = (char *) realloc(crawl->cachefile, needed);
-        if(!p)
-        {
-            return NULL;
-        }
-        crawl->cachefile = p;
-        crawl->cachefile_len = needed;
-    }
-    /* XXX create directory structure */
-    if(cache_filename(crawl, key, CACHE_INFO_SUFFIX, crawl->cachefile, needed) != needed)
-    {
-        return NULL;
-    }
-    return fopen(crawl->cachefile, "w");
+	if(cache_copy_filename_(crawl, key, CACHE_PAYLOAD_SUFFIX, 1))
+	{
+		return NULL;
+	}
+	if(cache_create_dirs_(crawl, crawl->cachefile))
+	{
+		return NULL;
+	}
+	return fopen(crawl->cachefile, "w");
 }
 
+int
+cache_close_info_rollback_(CRAWL *crawl, const CACHEKEY key, FILE *f)
+{
+	if(f)
+	{
+		fclose(f);
+		if(cache_copy_filename_(crawl, key, CACHE_INFO_SUFFIX, 1))
+		{
+			return -1;
+		}
+	}
+	return unlink(crawl->cachefile);
+}
+
+int
+cache_close_payload_rollback_(CRAWL *crawl, const CACHEKEY key, FILE *f)
+{
+	if(f)
+	{
+		fclose(f);
+		if(cache_copy_filename_(crawl, key, CACHE_PAYLOAD_SUFFIX, 1))
+		{
+			return -1;
+		}
+	}
+	return unlink(crawl->cachefile);
+}
+
+int
+cache_close_info_commit_(CRAWL *crawl, const CACHEKEY key, FILE *f)
+{
+	if(!f)
+	{
+		errno = EINVAL;
+		return -1;
+	}	
+	fclose(f);	
+	if(cache_copy_filename_(crawl, key, CACHE_INFO_SUFFIX, 1))
+	{
+		return -1;
+	}
+	if(cache_filename_(crawl, key, CACHE_INFO_SUFFIX, crawl->cachetmp, crawl->cachefile_len, 0) > crawl->cachefile_len)
+	{
+		errno = ENOMEM;
+		return -1;
+	}
+	return rename(crawl->cachefile, crawl->cachetmp);
+}
+
+int
+cache_close_payload_commit_(CRAWL *crawl, const CACHEKEY key, FILE *f)
+{
+	if(!f)
+	{
+		errno = EINVAL;
+		return -1;
+	}	
+	fclose(f);	
+	if(cache_copy_filename_(crawl, key, CACHE_PAYLOAD_SUFFIX, 1))
+	{
+		return -1;
+	}
+	if(cache_filename_(crawl, key, CACHE_PAYLOAD_SUFFIX, crawl->cachetmp, crawl->cachefile_len, 0) > crawl->cachefile_len)
+	{
+		errno = ENOMEM;
+		return -1;
+	}
+	return rename(crawl->cachefile, crawl->cachetmp);
+}
+
+static int
+cache_create_dirs_(CRAWL *crawl, const char *path)
+{
+	char *t;
+	struct stat sbuf;
+	
+	t = NULL;
+	for(;;)
+	{		
+		t = strchr((t ? t + 1 : path), '/');
+		if(!t)
+		{
+			break;
+		}
+		if(t == path)
+		{
+			/* absolute path: skip the leading slash */
+			continue;
+		}
+		strncpy(crawl->cachetmp, path, (t - path));
+		crawl->cachetmp[t - path] = 0;
+		if(stat(crawl->cachetmp, &sbuf))
+		{
+			if(errno != ENOENT)
+			{
+				return -1;
+			}
+		}
+		if(mkdir(crawl->cachetmp, 0777))
+		{
+			if(errno == EEXIST)
+			{
+				continue;
+			}
+		}
+	}
+	return 0;
+}
+
+static int
+cache_copy_filename_(CRAWL *crawl, const CACHEKEY key, const char *type, int temporary)
+{
+	size_t needed;
+	char *p;
+
+	needed = cache_filename_(crawl, key, type, NULL, 0, 1);
+	if(needed > crawl->cachefile_len)
+	{
+		p = (char *) realloc(crawl->cachefile, needed);
+		if(!p)
+		{
+		    return -1;
+		}
+		crawl->cachefile = p;
+		p = (char *) realloc(crawl->cachetmp, needed);
+		if(!p)
+		{
+		    return -1;
+		}
+		crawl->cachetmp = p;
+		crawl->cachefile_len = needed;
+	}
+	if(cache_filename_(crawl, key, type, crawl->cachefile, needed, temporary) > needed)
+	{
+		return -1;
+	}
+	return 0;
+}
